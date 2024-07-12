b324ba82a5df2a1205d636f1d775494e5fd35aba
Gabriel Rabelo Moura <gabriel.rbl.moura@gmail.com>
2024-07-12 00:35:04 -0300

    Structs

--- TPS/Diario_de_Classe.c
+++ TPS/Diario_de_Classe.c
@@ -2,12 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct
+{
+    char nome[100];
+    unsigned int matricula;
+
+    int notas[3];
+
+} aluno;
+
+typedef struct
+{
+    char codigo[10];
+    char nome[100];
+
+    aluno alunos[6];
+
+} turma;
+
+typedef struct
+{
+    char nome[100];
+    unsigned int registro;
+
+    turma turmas[2];
+
+} prof;
+
+int Exibir_prof()
+{
+}
+
+int Exibir_aluno()
+{
+}
+
+int Inserir_aluno()
+{
+}
+
+int Lancar_notas()
+{
+}
+
+int Exibir_turma()
+{
+}
+
+int Exibir_situacao()
+{
+}
+
+int Exportar()
+{
+}
+
 int main(int argc, char **argv)
 {
     if (argc < 3)
     {
-        printf("Tá faltando coisa");
-        return EXIT_FAILURE;
+        printf("Está faltando coisa!");
+        return 0;
     }
 
     FILE *input = fopen(argv[1], "r");
