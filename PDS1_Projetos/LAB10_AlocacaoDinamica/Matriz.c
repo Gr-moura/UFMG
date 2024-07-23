8d82e4a1f4ec9f64eebfc95e23849e77cc2c1b26
Gabriel Rabelo Moura <gabriel.rbl.moura@gmail.com>
2024-07-23 19:17:15 -0300

    Lab 10.2 Feito

---
+++ PDS1_Projetos/LAB10_AlocacaoDinamica/Matriz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int linhas, colunas;
+    scanf("%d %d", &linhas, &colunas);
+
+    for (int i = 0; i < linhas; i++)
+    {
+
+        for (int j = 0; j < colunas; j++)
+        {
+        }
+    }
+
+    return 0;
+}
