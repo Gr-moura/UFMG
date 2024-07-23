dfe2cc93a5b3540c5036db6ba9358f42f4fb0a52
Gabriel Rabelo Moura <gabriel.rbl.moura@gmail.com>
2024-07-23 20:24:42 -0300

    Corrigido Lab 10.2

--- PDS1_Projetos/LAB10_AlocacaoDinamica/Matriz.c
+++ PDS1_Projetos/LAB10_AlocacaoDinamica/Matriz.c
@@ -6,13 +6,32 @@ int main()
     int linhas, colunas;
     scanf("%d %d", &linhas, &colunas);
 
+    int **mat = (int **)malloc(linhas * sizeof(int *));
+
     for (int i = 0; i < linhas; i++)
     {
-
+        mat[i] = (int *)malloc(colunas * sizeof(int));
         for (int j = 0; j < colunas; j++)
         {
+            scanf("%d", &mat[i][j]);
         }
     }
 
-    return 0;
+    for (int i = 0; i < linhas; i++)
+    {
+        for (int j = 0; j < colunas; j++)
+        {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < linhas; i++)
+    {
+        free(mat[i]);
+    }
+
+    free(mat);
+
+    return EXIT_SUCCESS;
 }
