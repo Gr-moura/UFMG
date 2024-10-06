76c22f514176dd90a9fcddb564d3492e18ca6d7b
Gabriel Rabelo <163333836+Gr-moura@users.noreply.github.com>
2024-10-06 01:53:34 -0300

    Finalizado VPL 1

--- PDS2_Projetos/VPL1_Wordle.cpp
+++ PDS2_Projetos/VPL1_Wordle.cpp
@@ -1,9 +1,8 @@
 #include <fstream>
 #include <iostream>
-#include <sstream>
 using namespace std;
 
-string *Encontrar_Objetivo(ifstream &arquivo)
+string Encontrar_Objetivo(ifstream &arquivo)
 {
     int qt_palavras;
     arquivo >> qt_palavras;
@@ -11,12 +10,52 @@ string *Encontrar_Objetivo(ifstream &arquivo)
     int num_buscada;
     cin >> num_buscada;
 
-    string *obj = new string;
+    string chave;
     for (int i = 0; i < num_buscada; i++)
-        arquivo >> *obj;
+        arquivo >> chave;
 
-    cout << *obj;
-    return obj;
+    return chave;
+}
+
+bool Jogar(const string &chave)
+{
+    string letras;
+    for (int i = 0; i < 5; i++)
+    {
+        string chute;
+        cin >> chute;
+
+        if (chute == chave)
+        {
+            cout << chute;
+            cout << " (" << letras << ")" << endl;
+            return 1;
+        }
+
+        for (int j = 0; j < 5; j++)
+        {
+            if (chave[j] == chute[j])
+            {
+                cout << chave[j];
+                continue;
+            }
+
+            if (chave.find(chute[j]) != string::npos)
+            {
+                cout << (char)tolower(chute[j]);
+                continue;
+            }
+
+            if (letras.find(chute[j]) == string::npos)
+                letras = letras + chute[j];
+
+            cout << "*";
+        }
+
+        cout << " (" << letras << ")" << endl;
+    }
+
+    return 0;
 }
 
 int main()
@@ -28,9 +67,16 @@ int main()
         return 1;
     }
 
-    string *obj = Encontrar_Objetivo(arquivo);
+    string chave = Encontrar_Objetivo(arquivo);
 
-    delete obj;
+    bool venceu = Jogar(chave);
+
+    if (venceu)
+        cout << "GANHOU!" << endl;
+
+    else
+        cout << "PERDEU! " << chave << endl;
+
     arquivo.close();
     return 0;
 }
