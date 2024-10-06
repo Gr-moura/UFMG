8789aa8535d8b2fc0f734ff31ab0d35f5a3766f8
Gabriel Rabelo <163333836+Gr-moura@users.noreply.github.com>
2024-10-06 00:24:22 -0300

    Added Find_Objective function

--- PDS2_Projetos/VPL1_Wordle.cpp
+++ PDS2_Projetos/VPL1_Wordle.cpp
@@ -3,24 +3,34 @@
 #include <sstream>
 using namespace std;
 
+string *Encontrar_Objetivo(ifstream &arquivo)
+{
+    int qt_palavras;
+    arquivo >> qt_palavras;
+
+    int num_buscada;
+    cin >> num_buscada;
+
+    string *obj = new string;
+    for (int i = 0; i < num_buscada; i++)
+        arquivo >> *obj;
+
+    cout << *obj;
+    return obj;
+}
+
 int main()
 {
-    ifstream palavras("palavras.txt", fstream::in);
-    if (!palavras.is_open())
+    ifstream arquivo("palavras.txt", fstream::in);
+    if (!arquivo.is_open())
     {
-        cout << "Error: Palavras.txt";
+        cerr << "Error: Palavras.txt" << endl;
         return 1;
     }
 
-    int n;
-    palavras >> n;
+    string *obj = Encontrar_Objetivo(arquivo);
 
-    string *dados = new string[n];
-    cout << n;
-
-    string a;
-    palavras >> a;
-    cout << a;
-
-    delete[] dados;
+    delete obj;
+    arquivo.close();
+    return 0;
 }
