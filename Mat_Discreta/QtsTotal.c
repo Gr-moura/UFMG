d7f279b76ef10a9740ced7b88b9722a642cb2d9b
Gabriel Rabelo <163333836+Gr-moura@users.noreply.github.com>
2024-11-03 00:16:23 -0300

    Added QtsTotal.c and counts for segments & symbols

--- Mat_Discreta/QtsTotal.c
+++ Mat_Discreta/QtsTotal.c
@@ -3,25 +3,25 @@
 #define X 0
 #define Y 1
 
-const char koch[] = "F++FF--F+F";
+const char koch[] = "F-F++FF--F+F";
 const char correto[] = "F-F++FF--F+F";
 
-const char hilbert[2][12] = {"-YF+XFX+FY-", "+XF-YFY-FX+"};
+char hilbert[2][12] = {"-YF+XFX+FY-", "+XF-YFY-FX+"};
+
+int Signos = 0, Fs = 0;
+int somarSignos = 0, somarFs = 0;
 
 void Floco_de_Neve_Onda_Senoidal_1_de_Von_Koch(int ordem, int atual)
 {
-    // Axioma: F
-    // θ = π/3
-    //
-    // Regra: F → F-F+F+FF-F-F+F
-
     if (ordem == 0)
     {
-        printf("%s", koch);
+        printf("%s", koch); // t(0) = T
+        Signos += Signos;
+        Fs += somarFs;
         return;
     }
 
-    while (koch[atual]!='\0')
+    while (koch[atual] != '\0') // t(n) = R * t(n - 1) + nR
     {
         if (koch[atual] == 'F')
         {
@@ -30,24 +30,22 @@ void Floco_de_Neve_Onda_Senoidal_1_de_Von_Koch(int ordem, int atual)
             continue;
         }
 
-        printf("%c", koch[atual++]);
+        printf("%c", koch[atual++]); // t(n) = R^n * T + nR(R^n - 1) / (R - 1)
+
+        Signos++;
     }
 }
 
 void Preenchimento_de_Espaco_de_Hilbert(int ordem, int atual, int pai)
 {
-    // Axioma: X
-    // θ = π/2
-    //
-    // Regras :
-    // X → -YF+XFX+FY-
-    // Y → +XF-YFY-FX+
-
     if (ordem == 0)
     {
+        Signos += somarSignos;
+        Fs += somarFs;
+        
         if (pai == X)
         {
-            printf("%s", hilbert[X]);
+            printf("%s", hilbert[X]); 
             return;
         }
 
@@ -72,15 +70,34 @@ void Preenchimento_de_Espaco_de_Hilbert(int ordem, int atual, int pai)
         }
 
         printf("%c", hilbert[pai][atual++]);
+        Signos++;
+        
+        if (hilbert[pai][atual] == 'F')
+        {
+            Fs++;
+        }
     }
 }
 
 int main()
 {
+    char *regra = hilbert[X];
+
+    for (int i = 0; regra[i] != '\0'; i++)
+    {
+        somarSignos++;
+
+        if (regra[i] == 'F')
+        {
+            somarFs++;
+        }
+    }
+
     // t(n) = R^n * T + nR(R^n - 1) / (R - 1)
 
-    Floco_de_Neve_Onda_Senoidal_1_de_Von_Koch(1, 0);
-    Preenchimento_de_Espaco_de_Hilbert(1, 0, X);
+    // Floco_de_Neve_Onda_Senoidal_1_de_Von_Koch(2, 0);
+    Preenchimento_de_Espaco_de_Hilbert(2, 0, X);
+    printf("\n%d %d %d", Signos, Fs, Signos - Fs);
 
     return 0;
 }
---
+++ Testes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string a = "F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F-F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F++F-F++F-F-"
+               "F-F++F-F-F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F-F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F-F-F++F-F-F-F++F-F++"
+               "F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F-F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F++F-F++FF-"
+               "F-F++F-F++F-F++F-F-F-F++F-F-F-F++F-F-F-F++F-F++FF++F-F-F-F++F-F++F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F-"
+               "F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F-F-F++F-F-FF++F-F++F-F+"
+               "+F-F-F-F++F-F-F-F++F-F-F-F++F-F++F-F++F-F-F-"
+               "F++F-F++F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F-F-F++F-F-F-"
+               "F++F-F++F-F++F-F-F-F++F-F";
+    cout << a.length();
+    return 0;
+}
