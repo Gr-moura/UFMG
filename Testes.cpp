f062c8ad662c75f0e0fa7c83aee7be81c8a3729a
Gabriel Rabelo <163333836+Gr-moura@users.noreply.github.com>
2024-11-18 11:08:58 -0300

    Added debugger

--- Testes.cpp
+++ Testes.cpp
@@ -1,11 +1,42 @@
-#include <iostream>
-#include <string>
-using namespace std;
+
+#include <stdio.h>
+#include <string.h>
+
+void quantos(char *str, char c, int tamanho, char placeholder)
+{
+    if (tamanho > 1)
+        quantos(str, c, tamanho - 1, placeholder);
+
+    if (str[tamanho - 1] == c)
+        str[tamanho - 1] = placeholder;
+}
+
+void printar(char *str, char placeholder)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != placeholder)
+            printf("%c", str[i]);
+    }
+
+    printf("\n");
+}
 
 int main()
 {
-    string a = "F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F-F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F++F-F++F-F-"
-               "F-F++F-F-F-F++F-F-F-F++F-F++F-F++F-F-F-F++F-F";
-    cout << a.length();
+    char buffer[100], str[100];
+    char caractere;
+
+    fgets(buffer, 100, stdin);
+    sscanf(buffer, "%s", str);
+
+    fgets(buffer, 100, stdin);
+    sscanf(buffer, "%c", &caractere);
+
+    char placeholder = '%';
+
+    quantos(str, caractere, strlen(str), placeholder);
+    printar(str, placeholder);
+
     return 0;
 }
