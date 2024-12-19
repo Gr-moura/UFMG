75c9150d0372b1a52e453f0347db5487ad28ed20
Gabriel Rabelo <163333836+Gr-moura@users.noreply.github.com>
2024-12-18 21:14:38 -0300

    Added VPL 9

---
+++ Semestre_II/PDS_II/VPL9/vetor.hpp
@@ -0,0 +1,82 @@
+#ifndef VETOR_HPP
+#define VETOR_HPP
+
+template <typename T> class Node
+{
+  private:
+    T value;
+    Node<T> *next = nullptr;
+
+  public:
+    Node() : Node(0) {}
+    Node(T value) : value(value), next(nullptr) {}
+
+    const T getValue() const { return value; };
+    void setValue(T newValue) { this->value = newValue; };
+
+    const Node<T> *getNext const(return this;);
+    void setNext(Node<T> *newNext) { this -> next = newNext };
+};
+
+template <typename T> class Lista
+{
+  private:
+    Node<T> *head = nullptr;
+    Node<T> *tail = nullptr;
+
+  public:
+    void addBack(T value)
+    {
+        Node<T> *temp = new Node(value);
+
+        if (!temp)
+            return;
+
+        if (!head)
+        {
+            head = temp;
+            tail = temp;
+            return;
+        }
+
+        tail->setNext(temp);
+        tail = tail->getNext();
+    }
+
+    ~Lista()
+    {
+        Node<T> *temp = head;
+        Node<T> *next = nullptr;
+
+        while (temp)
+        {
+            next = temp->getNext();
+            delete temp;
+            temp = next;
+        }
+
+        head = nullptr;
+        tail = nullptr;
+    }
+};
+
+template <typename T> class Vetor
+{
+  private:
+    int maxSize;
+    int nElementos;
+
+  public:
+    Vetor(int n) : maxSize(n) {}
+    Vetor(const Vetor &copia) {}
+
+    ~Vetor() {}
+
+    SetElemento(int i, T x) {}
+    GetElemento(int i) {}
+
+    AdicionaElemento(T i) {}
+    Imprime() {}
+};
+
+#endif
