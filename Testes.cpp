21e7ff4c674ad9f584cbf8497d7eb884673643d3
Gabriel Rabelo <163333836+Gr-moura@users.noreply.github.com>
2025-01-07 19:09:20 -0300

    Corrigido Lab 12.3 - Agenda Binaria

--- Testes.cpp
+++ Testes.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 using namespace std;
-int *B;
-
-void passar(const int &A)
+class Base1
 {
-    cout << A;
-}
-
+  public:
+    Base1() { cout << "Base1()" << " "; }
+    virtual void Imprimir() = 0;
+};
+class Base2
+{
+  public:
+    Base2() { cout << "Base2()" << " "; }
+};
+class Derived : public Base1, public Base2
+{
+  public:
+    Derived() { cout << "Derived()" << " "; }
+    void Imprimir() { cout << 3; }
+};
 int main()
 {
-    int A = 5;
-    passar(A);
-    cout << A;
-
+    Derived d;
     return 0;
 }
